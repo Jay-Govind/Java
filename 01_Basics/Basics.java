4f6966041acf6b60e52396cee68531dc1633aa8e
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-02 22:37:01 +0530

    Data types

---
+++ 01_Basics/Basics.java
@@ -0,0 +1,21 @@
+// Print Using Println
+
+// public class Basics {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World !");
+//         System.out.println("Hello World !");
+
+//     }
+// }
+
+
+// Print Using - \n
+
+public class Basics {
+    public static void main(String[] args) {
+        System.out.print("* * * *\n");
+        System.out.print("* * * \n");
+        System.out.print("* * \n");
+        System.out.print("* \n");
+    }
+}
