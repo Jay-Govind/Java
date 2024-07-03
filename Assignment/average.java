4a0de64672f6257f96000852bdfe4f2dcc8ccbe4
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-04 00:33:33 +0530

    Average of three number

---
+++ Assignment/average.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class average {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        float num1 = sc.nextFloat();
+        System.out.print("Enter the second number : ");
+        float num2 = sc.nextFloat();
+        System.out.print("Enter the third number : ");
+        float num3 = sc.nextFloat();
+
+        float average = (num1 + num2 + num3) / 3; 
+        
+        System.out.println("Average of three numbers : " + average);
+        sc.close();
+    }
+}
