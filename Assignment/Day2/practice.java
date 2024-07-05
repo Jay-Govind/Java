92d5c2a4d04043400be47ff7d5f1732b82bd2760
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-05 12:07:03 +0530

    Practice Assignment Day2

--- Assignment/Day2/practice.java
+++ Assignment/Day2/practice.java
@@ -1,4 +1,5 @@
 package Day2;
+
 public class practice {
     public static void main(String[] args) {
         int x = 2, y = 5;
@@ -7,4 +8,50 @@ public class practice {
         System.out.print(exp1 + " , ");
         System.out.print(exp2);
     }
+    /* Output --> 5 , 4 */
+
+    // public static void main(String[] args)
+    {
+        int x = 200, y = 50, z = 100;
+        if (x > y && y > z) {
+            System.out.println("Hello");
+        }
+        if (z > y && z < x) {
+            System.out.println("Java");
+        }
+        if ((y + 200) < x && (y + 150) < z) {
+            System.out.println("Hello Java");
+        }
+    }
+    /* Output --> Java */
+
+    // public static void main(String[] args)
+    {
+        int x, y, z;
+        x = y = z = 2;
+        x += y;
+        y -= z;
+        z /= (x + y);
+        System.out.println(x + " " + y + " " + z);
+    }
+    /* Output --> 4 0 0  */
+
+    // public static void main(String[] args)
+    {
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+        int exp = 4 / 3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b * y);
+        System.out.println(exp);
+    }
+    /* Output --> 278 */
+
+    // public static void main(String[] args)
+    {
+        int x = 10, y = 5;
+        int exp1 = (y * (x / y + x / y));
+        int exp2 = (y * x / y + y * x / y);
+        System.out.println(exp1);
+        System.out.println(exp2);
+    }
+    /* Output --> 20 20 */
 }
