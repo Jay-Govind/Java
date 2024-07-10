b8ce8bd719f3336b98503eff50611fdcadce4ded
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-10 23:32:10 +0530

    initial code for continue statement

---
+++ 04_Basics/continueQuestion.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class continueQuestion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("Enter the number : ");
+            int num = sc.nextInt();
+            if (num % 10 == 0) {
+                continue;
+            }
+            System.out.println("Number is : " + num + " ");
+        } while (true);
+
+        // sc.close();
+    }
+}
+
+// in this code, if user input multiple of 10, then it will continue
