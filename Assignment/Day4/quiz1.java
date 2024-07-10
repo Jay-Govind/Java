38097115357fed0e52ffe358b276bedecb347177
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-10 20:44:01 +0530

    First Quiz

---
+++ Assignment/Day4/quiz1.java
@@ -0,0 +1,17 @@
+package Day4;
+
+import java.util.*;
+
+public class quiz1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for ( int i =0; i<5; i++){
+            System.out.println("Hello World");
+            i +=2;
+        }
+        sc.close();
+    }
+}
+
+// Output -> Hello World will be printed 2 times
