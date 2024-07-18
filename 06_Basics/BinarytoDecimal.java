e81d4e0bd85eb11e256674ba42e17acc8b298d85
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-18 22:18:45 +0530

    add input validation

--- 06_Basics/BinarytoDecimal.java
+++ 06_Basics/BinarytoDecimal.java
@@ -5,21 +5,42 @@ public class BinarytoDecimal {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the binary number : ");
-        int binaryNum = sc.nextInt();
+        long binaryNum = sc.nextLong();
 
-        binaryToDecimal(binaryNum);
+        if (binaryNum < 0 || !isValidBinary(binaryNum)) {
+            System.out.println("Invalid binary number. Binary numbers can only contain 0s and 1s.");
+        } else {
+            binaryToDecimal(binaryNum);
+        }
 
         sc.close();
     }
 
-    public static void binaryToDecimal(int binaryNum) {
-        int decimalNum = 0;
-        int power = 0;
+    public static boolean isValidBinary(long binaryNum) {
+        long temp = binaryNum;
 
-        int originalBinaryNum = binaryNum;
+        if (temp == 0) {
+            return true;
+        }
+
+        while (temp != 0) {
+            long digit = temp % 10;
+            if (digit != 0 && digit != 1) {
+                return false;
+            }
+            temp /= 10;
+        }
+        return true;
+    }
+
+    public static void binaryToDecimal(long binaryNum) {
+        long decimalNum = 0;
+        long power = 0;
+        long originalBinaryNum = binaryNum;
+
         while (binaryNum > 0) {
-            int lastDigit = binaryNum % 10;
-            decimalNum = decimalNum + (lastDigit * (int) Math.pow(2, power));
+            long lastDigit = binaryNum % 10;
+            decimalNum += (lastDigit * (long) Math.pow(2, power));
             power++;
             binaryNum /= 10;
 
