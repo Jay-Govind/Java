fec03e95c77d6cd30ad2e5eb06c455f2d0dfc2c9
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-19 00:32:33 +0530

    Inverted number triangle

--- 07_AdvancePatterns/InvertedNumberTriangle.java
+++ 07_AdvancePatterns/InvertedNumberTriangle.java
@@ -4,22 +4,17 @@ public class InvertedNumberTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // input
         System.out.print("Enter the number : ");
         int num = sc.nextInt();
 
-        int total_number_of_line = num;
-        int current_number_of_line = 1;
+        for (int current_line = 1; current_line <= num; current_line++) {
 
-        while (current_number_of_line <= total_number_of_line) {
-
-            for (int row = 1; row <= num; row++) {
-                System.out.print(row + " ");
+            for (int number = 1; number <= num; number++) {
+                System.out.print(number + " ");
             }
 
             System.out.println();
             num--;
-            current_number_of_line++;
         }
 
         sc.close();
