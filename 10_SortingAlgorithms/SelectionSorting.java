90e6b434a11d9fc5d001e82883efacc8b3d93fb0
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-24 15:44:35 +0530

    Selection sort

---
+++ 10_SortingAlgorithms/SelectionSorting.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class SelectionSorting {
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = (i + 1); j < arr.length-1; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many number you want to store ? ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter the " + (i + 1) + " element : ");
+            arr[i] = sc.nextInt();
+        }
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array : ");
+        System.out.println(Arrays.toString(arr));
+
+        sc.close();
+    }
+}
