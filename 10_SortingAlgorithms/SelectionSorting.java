77472bdfd356577c778dafc0202b0eda8d298ee9
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-24 15:45:55 +0530

    update

--- 10_SortingAlgorithms/SelectionSorting.java
+++ 10_SortingAlgorithms/SelectionSorting.java
@@ -3,12 +3,17 @@ import java.util.*;
 public class SelectionSorting {
 
     public static void selectionSort(int[] arr) {
+
         for (int i = 0; i < arr.length - 1; i++) {
+
             int minPos = i;
-            for (int j = (i + 1); j < arr.length-1; j++) {
+
+            for (int j = (i + 1); j < arr.length - 1; j++) {
+
                 if (arr[minPos] > arr[j]) {
                     minPos = j;
                 }
+
             }
 
             int temp = arr[i];
