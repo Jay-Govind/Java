2eee5bba374d6e430d6e403a3dd4578d1bb35e73
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-26 00:58:23 +0530

    update new functions

--- 11_2DArray/Matrix.java
+++ 11_2DArray/Matrix.java
@@ -14,6 +14,61 @@ public class Matrix {
         }
     }
 
+    public static boolean search(int[][] matrix, int key) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("| " + key + " found at " + (i + 1) + " row " + (j + 1) + " column |");
+                    return true;
+                }
+            }
+        }
+
+        System.out.println("Not found\n");
+        return false;
+    }
+
+    public static void largestInArray(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int max = matrix[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                    ;
+                }
+            }
+        }
+
+        System.out.println("| " + max + " is the largest element |");
+    }
+
+    public static void smallestInArray(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int min = matrix[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+
+        System.out.println("| " + min + " is the smallest element |");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -32,8 +87,10 @@ public class Matrix {
             }
         }
 
-        print(matrix);  // print the matrix
-        
+        print(matrix); // print the matrix
+        search(matrix, 6); // search an element from the array
+        largestInArray(matrix);
+        smallestInArray(matrix);
 
         sc.close();
     }
