eba98ec6edd566934909aa93375d1aedb8f85cef
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-26 00:45:17 +0530

    2D array

---
+++ 11_2DArray/Matrix.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Matrix {
+    public static void print(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows : ");
+        int m = sc.nextInt();
+
+        System.out.print("Enter the number of columns : ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("Enter the " + (i + 1) + " row " + (j + 1) + " column element : ");
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        print(matrix);  // print the matrix
+        
+
+        sc.close();
+    }
+}
