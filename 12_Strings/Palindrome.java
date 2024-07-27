a41074ade09fe125d69a056d068417eb63713372
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-27 23:16:02 +0530

    update the code

--- 12_Strings/Palindrome.java
+++ 12_Strings/Palindrome.java
@@ -4,11 +4,11 @@ public class Palindrome {
     public static boolean isPalindrome(String str) {
         int n = str.length();
         for (int i = 0; i < n / 2; i++) {
-            if (str.charAt(i) == str.charAt(n - 1 - i)) {
-                return true;
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
