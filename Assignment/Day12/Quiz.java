0cecb58d510950fff90759a63f737b359ce6df76
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-01 12:18:40 +0530

    Quiz of assignment

---
+++ Assignment/Day12/Quiz.java
@@ -0,0 +1,18 @@
+package Day12;
+
+public class Quiz {
+    // public static void main(String args[]) {
+    // String str = "ShradhaDidi";
+    // String str1 = "ApnaCollege";
+    // String str2 = "ShradhaDidi";
+    // System.out.println(str.equals(str1) + " " + str.equals(str2));
+    // }
+    // output : false true
+
+    public static void main(String args[]) {
+        String str = "ApnaCollege".replace("l", "");
+        System.out.println(str);
+    }
+    // output : ApnaCoege
+
+}
