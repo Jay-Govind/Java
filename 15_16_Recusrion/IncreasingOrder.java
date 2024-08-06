ce708b6c16ff018a0ae55b45a068724d359c4331
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-06 23:13:00 +0530

    Print number in increasing order

---
+++ 15_16_Recusrion/IncreasingOrder.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class IncreasingOrder {
+
+    public static void increasingOrder(int num) {
+        if (num == 0)
+            return;
+
+        increasingOrder(num - 1);
+        System.out.print(num + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+
+        increasingOrder(num);
+
+        sc.close();
+    }
+}
