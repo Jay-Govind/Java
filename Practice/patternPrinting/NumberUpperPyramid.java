71ada1211885b6e2072cd49da8a36ac5b2337fc9
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-21 23:09:31 +0530

    number upper pyramid

---
+++ Practice/patternPrinting/NumberUpperPyramid.java
@@ -0,0 +1,21 @@
+package Practice.patternPrinting;
+
+import java.util.*;
+
+public class NumberUpperPyramid {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of lines : ");
+        int lines = sc.nextInt();
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < lines-i; j++) {
+                System.out.print((j + 1) + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
