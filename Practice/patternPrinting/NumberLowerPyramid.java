c8e61f2913b1619b6da6a4706ccb4b540ec8ae63
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-21 23:05:21 +0530

    Number pyramid

--- Practice/patternPrinting/NumberLowerPyramid.java
+++ Practice/patternPrinting/NumberLowerPyramid.java
@@ -11,7 +11,7 @@ public class NumberLowerPyramid {
 
         for (int i = 0; i < lines; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print((j+1) + " ");
+                System.out.print((j + 1) + " ");
             }
             System.out.println();
         }
