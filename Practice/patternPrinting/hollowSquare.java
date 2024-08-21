8024b0d0f952d9de4fd78e9a07f2addd0319ef97
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-21 23:25:55 +0530

    hollow square

--- Practice/patternPrinting/hollowSquare.java
+++ Practice/patternPrinting/hollowSquare.java
@@ -1,7 +1,7 @@
 package Practice.patternPrinting;
 import java.util.*;
 
-public class hollowSquare {
+public class HollowSquare {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
