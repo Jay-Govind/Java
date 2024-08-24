3b17ac94765a1d4399831ddc754d703d7b9a9f89
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-24 20:11:34 +0530

    modify the code for prime number

--- Practice/loops/Prime.java
+++ Practice/loops/Prime.java
@@ -6,28 +6,37 @@ public class Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number : ");
-        int num = sc.nextInt();
+        int num;
 
-        boolean isPrime = true;
+        do {
+            System.out.print("Enter a number : ");
+            num = sc.nextInt();
 
-        if (num == 1) {
-            System.out.println("1 is neither prime nor composite");
-        } else if (num == 2) {
-            System.out.println("2 is a prime number");
-        } else {
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    isPrime = false;
-                    break;
+            if (num > 0) {
+                boolean isPrime = true;
+
+                if (num == 1) {
+                    System.out.println("1 is neither prime nor composite");
+                } else if (num == 2) {
+                    System.out.println("2 is a prime number");
+                } else {
+                    for (int i = 2; i <= Math.sqrt(num); i++) {
+                        if (num % i == 0) {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime == true) {
+                        System.out.println(num + " is a prime number");
+                    } else {
+                        System.out.println(num + " is a composite number");
+                    }
                 }
-            }
-            if (isPrime == true) {
-                System.out.print(num + " is a prime number");
             } else {
-                System.out.print(num + " is a composite number");
+                System.out.println("Invalid Input, Try Again");
             }
-        }
+
+        } while (num <= 0);
         sc.close();
     }
 }
