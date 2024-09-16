28f2394ae58b39bdffe53e4b0d93000fd8d29b4a
Jay Govind Kumar <govind.iq@gmail.com>
2024-09-16 23:02:46 +0530

    selection sort

---
+++ Practice/Sorting/SelectionSort.java
@@ -0,0 +1,48 @@
+package Practice.Sorting;
+
+import java.util.*;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
+        }
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter the " + (i + 1) + " element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        selectionSort(arr);
+        printArr(arr);
+
+        sc.close();
+    }
+}
