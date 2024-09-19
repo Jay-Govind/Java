8675a06cf95e9b0362cd2ed590110cadf4fd4c52
Jay Govind Kumar <govind.iq@gmail.com>
2024-09-20 00:15:37 +0530

    insertion sort

---
+++ Practice/Sorting/InsertionSort.java
@@ -0,0 +1,47 @@
+package Practice.Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            // find the correct position to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+
+            // insertion
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter the " + (i + 1) + " element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        insertionSort(arr);
+        printArr(arr);
+
+        sc.close();
+    }
+}
