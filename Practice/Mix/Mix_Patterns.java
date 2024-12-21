7a67f81366c8cdd8a8fc51b1d53769f60eb63966
Jay Govind Kumar <govind.iq@gmail.com>
2024-12-21 14:11:39 +0530

    add the codes for star pattern

--- Practice/Mix/Mix_Patterns.java
+++ Practice/Mix/Mix_Patterns.java
@@ -6,8 +6,11 @@ public class Mix_Patterns {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // starPattern(sc);
-        // invertedStar(sc);
+        // upperStarPattern(sc);
+        // lowerStarPattern(sc);
+        // invertedUpperStarPattern(sc);
+        // invertedLowerStarPattern(sc);
+        numberPattern(sc);
 
         sc.close();
     }
@@ -16,7 +19,7 @@ public class Mix_Patterns {
     // * *
     // * * *
     // * * * *
-    public static void starPattern(Scanner sc) {
+    public static void upperStarPattern(Scanner sc) {
         System.out.print("Enter the number of lines: ");
         int num = sc.nextInt();
         for (int i = 0; i < num; i++) {
@@ -31,10 +34,46 @@ public class Mix_Patterns {
     // * * *
     // * *
     // *
-    public static void invertedStar(Scanner sc) {
+    public static void lowerStarPattern(Scanner sc) {
         System.out.print("Enter the number of lines: ");
         int num = sc.nextInt();
         for (int i = 1; i <= num; i++) {
+            for (int j = num; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    // - - - *
+    // - - * *
+    // - * * *
+    // * * * *
+    public static void invertedUpperStarPattern(Scanner sc) {
+        System.out.print("Enter the number of lines: ");
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            for (int k = 0; k < num - i - 1; k++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    // * * * *
+    // - * * *
+    // - - * *
+    // - - - *
+    public static void invertedLowerStarPattern(Scanner sc) {
+        System.out.print("Enter the number of lines: ");
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            for (int k = 1; k < i; k++) {
+                System.out.print("  ");
+            }
             for (int j = num; j >= i; j--) {
                 System.out.print("* ");
             }
