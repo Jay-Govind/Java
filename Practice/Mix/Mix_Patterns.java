79fc20acc6acc7a5a35ba692f5b843507004f74c
Jay Govind Kumar <govind.iq@gmail.com>
2024-12-24 21:46:14 +0530

    add the codes for Floyd's Triangle

--- Practice/Mix/Mix_Patterns.java
+++ Practice/Mix/Mix_Patterns.java
@@ -224,6 +224,24 @@ public class Mix_Patterns {
         }
     }
 
+    // 1
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+    // 11 12 13 14 15
+    public static void floydTrianglePattern(Scanner sc) {
+        System.out.print("Enter the numbetr of lines : ");
+        int num = sc.nextInt();
+        int counter = 1;
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -239,7 +257,8 @@ public class Mix_Patterns {
         // hollowSquarePattern(sc);
         // rectanglePattern(sc);
         // hollowRectanglePattern(sc);
-        zeroOneTrianglePattern(sc);
+        // zeroOneTrianglePattern(sc);
+        // floydTrianglePattern(sc);
 
         sc.close();
     }
