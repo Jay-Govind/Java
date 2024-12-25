82d467c05d7353a67bdf24926d4af101ab098b7b
Jay Govind Kumar <govind.iq@gmail.com>
2024-12-25 23:23:00 +0530

    add the code for obtaining binomial coefficients

--- Practice/Mix/Functions.java
+++ Practice/Mix/Functions.java
@@ -22,11 +22,44 @@ public class Functions {
         } while (num < 0);
     }
 
+    public static long factorialofGivenNumber(int num) {
+        long fact = 1;
+        if (num == 0 || num == 1) {
+            return fact;
+        } else {
+            for (int i = 1; i <= num; i++) {
+                fact *= i;
+            }
+            return fact;
+        }
+    }
+
+    public static void binomialCoefficient(Scanner sc) {
+
+        int n, r;
+        do {
+            System.out.print("Enter total number of items (n) : ");
+            n = sc.nextInt();
+            System.out.print("Enter number of items to choose (r) : ");
+            r = sc.nextInt();
+            if (n < r) {
+                System.out.println("n should be greater than or equal to r");
+            }
+        } while (n < r);
+
+        long factn = factorialofGivenNumber(n);
+        long factr = factorialofGivenNumber(r);
+        long factnr = factorialofGivenNumber(n - r);
+        long bc = factn / (factr * factnr);
+        System.out.println("Binomial Coefficient of " + n + " and " + r + " is " + bc);
+
+    }
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
         // factorial(sc);
-
+        binomialCoefficient(sc);
         sc.close();
     }
 }
