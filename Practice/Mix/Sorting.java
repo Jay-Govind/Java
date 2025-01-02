8ef4d092022c2d62f88395d8b4c7486dceb52c88
Jay Govind Kumar <govind.iq@gmail.com>
2025-01-02 06:35:17 +0530

    add the codes for bubble sort

---
+++ Practice/Mix/Sorting.java
@@ -0,0 +1,59 @@
+package Practice.Mix;
+
+import java.util.Scanner;
+
+public class Sorting {
+
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean isSorted = true;
+            for (int j = 0; j < (n - 1 - i); j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                return;
+            }
+        }
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter the " + (i + 1) + " element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        // bubbleSort(arr);
+        selectionSort(arr);
+
+        printArr(arr);
+
+        sc.close();
+    }
+}
