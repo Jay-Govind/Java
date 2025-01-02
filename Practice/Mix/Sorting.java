d082c46308929f72e30cdb8e32e78b8bdb55ee46
Jay Govind Kumar <govind.iq@gmail.com>
2025-01-02 11:35:15 +0530

    add the codes

--- Practice/Mix/Sorting.java
+++ Practice/Mix/Sorting.java
@@ -129,6 +129,20 @@ public class Sorting {
         }
     }
 
+    // Insertion Sort (Decreasing Order)
+    public static void insertionSort2(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] < curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
 
@@ -140,8 +154,8 @@ public class Sorting {
 
         // -----Decreasing Order-----
         // bubbleSort2(arr);
-        selectionSort2(arr);
-        // insertionSort2(arr);
+        // selectionSort2(arr);
+        insertionSort2(arr);
         // countingSort2(arr);
 
         printArr(arr);
