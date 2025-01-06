584517186aefb8314037e7165da994d6b33b1e4e
Jay Govind Kumar <govind.iq@gmail.com>
2025-01-06 14:17:48 +0530

    add the code files of string lecture assignments

--- Practice/Mix/Strings.java
+++ Practice/Mix/Strings.java
@@ -38,14 +38,72 @@ public class Strings {
         return (float) Math.sqrt(x * x + y * y);
     }
 
+    // Function to convert a first character of each word of a string to uppercase
+    public static String toUpperCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        StringBuilder newStr = new StringBuilder();
+
+        newStr.append(Character.toUpperCase(str.charAt(0)));
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i - 1) == ' ') {
+                newStr.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                newStr.append(str.charAt(i));
+            }
+        }
+        return newStr.toString();
+    }
+
+    // String Compression
+    public static String compress(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        StringBuilder newStr = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < (str.length() - 1) && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            newStr.append(str.charAt(i));
+            if (count > 1) {
+                newStr.append(count);
+            }
+        }
+        return newStr.toString();
+    }
+
+    // Print largest string in lexicographical order
+    public static String largestString(String[] str) {
+        String largest = str[0];
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].compareTo(largest) > 0) {
+                largest = str[i];
+            }
+        }
+        return largest;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
-        // system.out.println(isPalindrome(str));
+        // System.out.println(isPalindrome(str));
         // System.out.println(shortestPath(str));
+        // System.out.print(toUpperCase(str));
+        System.out.print(compress(str));
+
+        // String str2[] = { "mouse", "moniter", "mousepad" };
+        // System.out.println(largestString(str2));
+
         sc.close();
     }
 }
