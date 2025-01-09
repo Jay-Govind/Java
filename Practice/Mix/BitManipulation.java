58442799351e9fa17276ebed78c1a9dbf2940c5b
Jay-Govind <govind.iq@gmail.com>
2025-01-09 23:49:22 +0530

    add: add a new file for bit manipulation

---
+++ Practice/Mix/BitManipulation.java
@@ -0,0 +1,14 @@
+package Practice.Mix;
+
+import java.util.*;
+
+public class BitManipulation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(5 + 6); // 11
+        System.out.println(5 & 6); // 4
+
+        sc.close();
+    }
+}
