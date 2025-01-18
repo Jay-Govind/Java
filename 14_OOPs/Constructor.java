bf3a908a18e19433a213cc8ff30a14a2a0452938
Jay-Govind <govind.iq@gmail.com>
2025-01-18 13:09:20 +0530

    OOPs: Constructor Overloading Code

--- 14_OOPs/Constructor.java
+++ 14_OOPs/Constructor.java
@@ -1,24 +1,36 @@
 public class Constructor {
-    public static void main(String[] args){
-        // Student s1 = new Student("Jay", 15);
-        // Student s2 = new Student("Jay", 15);
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Jay Govind");
+        Student s3 = new Student(20);
+        Student s4 = new Student("Jay Govind", 20);
 
-        Student s3 = new Student();
-        Student s4 = new Student();
+        System.out.println(s1.name + " " + s1.age); // null 0
+        System.out.println(s2.name + " " + s2.age); // Jay Govind 0
+        System.out.println(s3.name + " " + s3.age); // null 20
+        System.out.println(s4.name + " " + s4.age); // Jay Govind 20
     }
 }
-class Student{
+
+class Student {
     String name;
     int age;
 
-    // Student(String name,int age){
-    //     this.name = name;
-    //     this.age = age;
-    //     System.out.println("Name : " + this.name );
-    //     System.out.println("Age : " + this.age);
-    // }
+    // Constructor overloading
+    Student() {
+        System.out.println("Constructor is calling...");
+    }
 
-    Student(){
-        System.out.println("Constructor is called...");
+    Student(String name) {
+        this.name = name;
+    }
+
+    Student(int age) {
+        this.age = age;
+    }
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
